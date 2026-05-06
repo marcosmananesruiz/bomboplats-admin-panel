038778156fecb23729327a09490bb03707b0260a
marcos.mananesruiz <marcos.mananesruiz@educa.madrid.org>
2026-05-06 23:45:23 +0200

    subir fotos funcional

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://bomboplats.run.place:8080',
   s3bucket: 'https://bomboplats-imagestorage.s3.us-east-1.amazonaws.com/',
 };
